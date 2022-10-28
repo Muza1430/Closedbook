eff706a40c74a43f0086ae53d0c705a1222d8f23
Mohammed muzafar <113344052+Muza1430@users.noreply.github.com>
2022-10-28 15:22:15 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,10 @@
+package Practicecodes.Java.Closedbook;
+
+public class Sample {
+    public static void main(String[] args) {
+        int i=-243;
+        int j=243;
+        if(i==j)/* ; *///Semi colon in this line ends the if there itself
+        System.out.println("Number are the same");
+    }
+}
